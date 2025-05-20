e8315c4acfeb951b46bbb327c98157618d9b9948
Alejandra-437 <00100520@uca.edu.sv>
2025-05-20 07:48:55 -0600

    :sparkles: feature/proyect-repository

---
+++ labos/src/main/java/com/cmas/labos/model/Usuario.java
@@ -0,0 +1,51 @@
+package com.cmas.labos.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String correo;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<Proyecto> proyectos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public List<Proyecto> getProyectos() {
+        return proyectos;
+    }
+
+    public void setProyectos(List<Proyecto> proyectos) {
+        this.proyectos = proyectos;
+    }
+
+}
