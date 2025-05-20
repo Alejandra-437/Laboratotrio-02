0305e84dbe67274e032a8bb69463eb6ce0181c25
Alejandra-437 <00100520@uca.edu.sv>
2025-05-20 07:41:26 -0600

    :sparkles: feature/base

---
+++ labos/src/test/java/com/cmas/labos/LabosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmas.labos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
