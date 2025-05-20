c356715724f5e1e102f76b81880df52e8a0c1046
Alejandra-437 <00100520@uca.edu.sv>
2025-05-20 07:54:24 -0600

    :sparkles: feature/tarea-user repository

---
+++ labos/src/main/java/com/cmas/labos/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.cmas.labos.repository;
+
+import com.cmas.labos.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
